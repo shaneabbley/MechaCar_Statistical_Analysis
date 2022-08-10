00eb182fa81e5b931223893cd87ecf53707d9421
shaneabbley <100976699+shaneabbley@users.noreply.github.com>
2022-08-10 13:08:00 -0700

    Deliverable 3 code added

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -14,3 +14,13 @@ total_summary <- suspension  %>% summarize(Mean=mean(PSI),Median=median(PSI),Var
 View(total_summary)
 lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),Standard_Deviation=sd(PSI), .groups = 'keep') #create summary table with summary stats grouped by lot
 View(lot_summary)
+
+# Deliverable 3
+t.test(suspension$PSI,mu=1500)
+lot1 <- subset(suspension, Manufacturing_Lot=="Lot1")
+t.test(lot1$PSI, mu=1500)
+lot2 <- subset(suspension, Manufacturing_Lot=="Lot2")
+t.test(lot2$PSI, mu=1500)
+lot3 <- subset(suspension, Manufacturing_Lot=="Lot3")
+t.test(lot3$PSI, mu=1500)
+
